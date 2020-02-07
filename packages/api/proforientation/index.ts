94c7f15ae67d31e32e39f15ec2b81f86740a81af
Jekins <jekinsd@gmail.com>
2020-02-07 18:19:52 +0300

    Git init

---
+++ packages/api/proforientation/index.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+}
+
+const baseURL = "https://jsonplaceholder.typicode.com/";
+
+const instance = axios.create({
+  baseURL
+});
+
+export const apiClient = {
+  posts: {
+    async getListing(): Promise<Post[]> {
+      const { data } = await instance.get("posts");
+      return data;
+    }
+  }
+};
